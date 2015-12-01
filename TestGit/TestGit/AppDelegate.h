380a2c5a401dac1c55b1e610ce0acc9833ef6cbe
haha <haha@qq.com>
2015-12-01 10:06:06 +0800

    testest

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by kittyryoko on 15/12/1.
+//  Copyright (c) 2015年 kittyryoko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by kittyryoko on 15/12/1.
+//  Copyright (c) 2015年 kittyryoko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
